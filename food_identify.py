9930eb54d24f353d44adb0444ccc493350e513d4
Tarun Narayanan <tarunnarayanan.v.2017.cse@rajalakshmi.edu.in>
2024-02-26 10:18:44 -0800

    Update food_identify.py

--- food_identify.py
+++ food_identify.py
@@ -27,9 +27,9 @@ st.session_state['api_key'] = st.secrets['OPENAI_KEY']
 # api_key_input = api_key_placeholder.text_input("Enter your OPENAI API Key", type="password", key="api_input")
 
 if st.button("Set API Key") or 'api_key' in st.session_state:
-    if api_key_input:
-        st.session_state['api_key'] = api_key_input
-        api_key_placeholder.empty()  # Remove the input box after setting the API key
+    # if api_key_input:
+        # st.session_state['api_key'] = api_key_input
+        # api_key_placeholder.empty()  # Remove the input box after setting the API key
         st.success("API Key is set. You can now upload images.")
 
         # Image Upload
